fda714b3a6ed614154155b4fd88172815c671208
Bhola116 <169025421+Bhola116@users.noreply.github.com>
2024-05-30 20:05:29 +0530

    Update start.py

--- GOKUMUSIC/plugins/bot/start.py
+++ GOKUMUSIC/plugins/bot/start.py
@@ -29,13 +29,13 @@ from Strings import get_string
 #--------------------------
 
 NEXI_VID = [
-"https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
-"https://graph.org/file/ba7699c28dab379b518ca.mp4",
-"https://graph.org/file/83ebf52e8bbf138620de7.mp4",
-"https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
+"https://telegra.ph/file/2d78da89d1fe2877569da.mp4",
+"https://telegra.ph/file/1b150c8e512b619689563.mp4",
+"https://telegra.ph/file/f448c76ad17b425d41314.mp4",
+"https://telegra.ph/file/2d6ff90f4fed535e0c920.mp4",
 "https://graph.org/file/318eac81e3d4667edcb77.mp4",
 "https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
-"https://graph.org/file/2a7f857f31b32766ac6fc.mp4",
+"https://graph.org/file/2a7f857f31b32766ac6fc.mp4
 
 ]
 
